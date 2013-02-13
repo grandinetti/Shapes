15bd7f318481829ab288c5c60ba1385a96caed6c
Shane Zatezalo <shane@lottadot.com>
2013-02-12 21:02:09 -0500

    Add iOS Example app

---
+++ ShapesiOSExample/ShapesiOSExample/ShapesiOSExampleTests/ShapesiOSExampleTests.h
@@ -0,0 +1,13 @@
+//
+//  ShapesiOSExampleTests.h
+//  ShapesiOSExampleTests
+//
+//  Created by Shane Zatezalo on 2/12/13.
+//  Copyright (c) 2013 Lottadot LLC. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ShapesiOSExampleTests : SenTestCase
+
+@end
